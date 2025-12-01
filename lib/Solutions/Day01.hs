7175246453e31166938dcb47a8392c825cb9c4e0
Love Lyckaro <love.lyckaro@tietoevry.com>
2025-12-01 06:28:37 +0100

    Solve day 1

--- lib/Solutions/Day01.hs
+++ lib/Solutions/Day01.hs
@@ -1,21 +1,45 @@
 module Solutions.Day01 where
 
-import Control.Arrow
 import Control.Monad.Error.Class
-import Control.Monad.IO.Class
+import Data.List (scanl')
 import Data.Text (Text)
 import Data.Text qualified as T
 import SantaLib hiding (part1, part2)
-import SantaLib.Parsing
+import SantaLib.Parsing hiding (count)
 
-pInp :: Parser Text
-pInp = symbol "beans"
+pDir :: Parser Int
+pDir = do
+  dir <- 1 <$ string "R" <|> (-1) <$ string "L"
+  amount <- decimal
+  return $ dir * amount
 
-part1 :: (MonadIO m, MonadError String m) => PartSolution m
-part1 = Solved $ parseErr pInp
+pInp :: Parser [Int]
+pInp = do
+  rotations <- some (lexemeLn pDir)
+  eof
+  return rotations
 
-part2 :: PartSolution m
-part2 = Unsolved
+sums :: [Int] -> [Int]
+sums = scanl' plusMod100 50
+  where
+    plusMod100 x y = (x + y) `mod` 100
 
-day01 :: (MonadIO m, MonadError String m) => Solution m
+count :: (a -> Bool) -> [a] -> Int
+count pred = length . filter pred
+
+part1 :: (MonadError String m) => PartSolution m
+part1 = Solved $ \inp -> do
+  nums <- parseErr pInp inp
+  return . T.show . count (== 0) . sums $ nums
+
+explode :: Int -> [Int]
+explode n = replicate (abs n) (signum n)
+
+part2 :: (MonadError String m) => PartSolution m
+part2 = Solved $ \inp -> do
+  nums <- parseErr pInp inp
+  let exploded = nums >>= explode
+  return . T.show . count (== 0) . sums $ exploded
+
+day01 :: (MonadError String m) => Solution m
 day01 = Solution part1 part2
